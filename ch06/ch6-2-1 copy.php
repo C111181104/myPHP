735cd009ddda807682d765eeb0fb537eb9da8fd8
C111181104 <C111181104@nkust.edu.tw>
2024-11-21 10:02:45 +0800

    ch6-2-1

---
+++ ch06/ch6-2-1 copy.php
@@ -0,0 +1,11 @@
+<?php
+function pageHeader(){
+   print"<h3>PHP與MySQL網頁設計</h3>";
+   echo "<hr/>";}
+function pageFooter(){print"<hr/>(c)Copyright by 劉桓均<br/>";}
+?>
+<?php
+pageHeader();
+print"<p>在PHP程式使用函數建立標題和註腳文字</p>";
+pageFooter();
+?>
